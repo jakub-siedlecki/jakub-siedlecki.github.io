63f32e521e9d634a0388176a00ea1e3750cda5d7
Jakub <jakub@ebest.pl>
2017-06-19 14:42:27 +0200

    portfolio mobile burger repair

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     var pos = $(id).offset().top;
     $('body, html').animate({scrollTop: pos});
 });
+
+$(document).ready(function () {
+// burger menu
+  menuContent = $('#navbar>li').clone();
+  $('.overlay-content>ul').prepend(menuContent);
+
+  menu = $('.menu');
+    burger = $("#burger");
+    topMenu = $('.menu-top');
+    menuMiddle = $('.menu-middle');
+    menuBottom = $('.menu-bottom');
+
+    menu.click(function(){
+        topMenu.toggleClass('menu-top-click');
+        menuMiddle.toggleClass('menu-middle-click');
+        menuBottom.toggleClass('menu-bottom-click');
+        burger.toggleClass('burger-active');
+    });
+    $(document).keyup(function(e) {
+
+        if (e.keyCode == 27 && burger.hasClass('burger-active')) {
+            burger.removeClass('burger-active');
+            topMenu.removeClass('menu-top-click');
+            menuMiddle.removeClass('menu-middle-click');
+            menuBottom.removeClass('menu-bottom-click');
+        }   // esc
+    });
+});
